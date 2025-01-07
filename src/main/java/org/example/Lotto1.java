022e9c54d4e1a04edeba9a16114a7299673a8c2d
Hyeonji Jeon <guswl1397@naver.com>
2025-01-07 17:30:08 +0900

    Initial commit

---
+++ src/main/java/org/example/Lotto1.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.concurrent.LinkedTransferQueue;
+
+public class Lotto1 {
+
+    public static void main(String[] args) {
+
+        //결과물을 담을 배열 선언
+        int[] result = new int[6];
+
+        for (int i = 0; i < 6; i++) {
+
+            int temp = (int)(Math.random()*45) +1;
+
+        //result배열안에 temp가 있는지 확인하는 로직을 구성
+
+
+
+            result[i] = temp;
+        }
+        //1 - 45 랜덤
+
+        System.out.println(Arrays.toString(result));
+
+    }
+}
