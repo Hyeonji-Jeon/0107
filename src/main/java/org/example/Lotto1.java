0b0899d89dec3b8e759da6ce048633462fc1f258
Hyeonji Jeon <guswl1397@naver.com>
2025-01-07 17:57:16 +0900

    Changes

--- src/main/java/org/example/Lotto1.java
+++ src/main/java/org/example/Lotto1.java
@@ -10,15 +10,22 @@ public class Lotto1 {
         //결과물을 담을 배열 선언
         int[] result = new int[6];
 
-        for (int i = 0; i < 6; i++) {
+        for (int i = 0; i < 6;  i++) {
 
             int temp = (int)(Math.random()*45) +1;
 
         //result배열안에 temp가 있는지 확인하는 로직을 구성
+            for (int j = 0; j < i; j++) {
 
-
+                if(result[j] == temp){
+                    System.out.println("중복 발생 : " + result[j]);
+                    continue;
+                
+                }
+            }
 
             result[i] = temp;
+
         }
         //1 - 45 랜덤
 
